08657900a288ab9254a7e6ae073cad97825f5f05
harinivassahana-max <harinivassahana@gmail.com>
2025-12-01 07:13:13 +1100

    assignment

--- activity 3.py
+++ activity 3.py
@@ -5,7 +5,7 @@ my_wn.title("Turtle")
 my_pen = turtle.Turtle()
 size = 0
 while True:
-    for i in range (4):
+    for i in range (0,4):
         my_pen.fd(size+1)
         my_pen.left(90)
         size = size-5
---
+++ assignment 14.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.Screen().bgcolor("aqua")
+board = turtle.Turtle()
+
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+
+turtle.done()
